2343e313b546a9ef3ce073300d68cb9a11ad5b6e
Dean Ivanov <dean.ivanov96@gmail.com>
2023-04-24 16:53:16 +0300

    Controllers Updated

--- src/main/java/com/example/carrental/controllers/CarController.java
+++ src/main/java/com/example/carrental/controllers/CarController.java
@@ -2,6 +2,7 @@ package com.example.carrental.controllers;
 
 import com.example.carrental.models.Car;
 import com.example.carrental.services.CarService;
+import com.example.carrental.services.LocationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,10 +16,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class CarController {
 
     private CarService carService;
+    private LocationService locationService;
 
     @Autowired
-    public CarController(CarService carService) {
+    public CarController(CarService carService, LocationService locationService) {
         this.carService = carService;
+        this.locationService = locationService;
     }
 
     @GetMapping(value = "/car-register")
@@ -27,8 +30,9 @@ public class CarController {
     }
 
     @PostMapping(value = "/car-register")
-    public String registerUser(@Valid @ModelAttribute("car") Car car,
+    public String registerUser(@Valid @ModelAttribute("car") Car car, @RequestParam("location") String locationName,
                                @RequestParam("file") MultipartFile multipart) {
+        car.setLocation(locationService.getLocation(locationName).get(0));
         carService.create(car.getId(), car, multipart);
         return "index";
     }
