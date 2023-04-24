a303f075688dd70be48d68640f2748a7f999afd8
Dean Ivanov <dean.ivanov96@gmail.com>
2023-04-24 20:54:14 +0300

    Quick Fixes

--- src/main/java/com/example/carrental/controllers/OrderController.java
+++ src/main/java/com/example/carrental/controllers/OrderController.java
@@ -1,6 +1,9 @@
 package com.example.carrental.controllers;
 
+import com.example.carrental.models.Car;
 import com.example.carrental.models.Order;
+import com.example.carrental.models.Payment;
+import com.example.carrental.models.User;
 import com.example.carrental.services.LocationService;
 import com.example.carrental.services.OrderService;
 import jakarta.validation.Valid;
@@ -24,14 +27,14 @@ public class OrderController {
     public String newOrder(@Valid @ModelAttribute("order")Order order, @RequestParam("orderLocation") String locationName) {
         order.setLocation(locationService.getLocation(locationName).get(0));
 
-        order.setCar(null);
-        order.setUser(null);
+        order.setCar(new Car());
+        order.setUser(new User());
         order.setStartDate(order.getStartDate());
         order.setEndDate(order.getEndDate());
         order.setLocation(locationService.getLocation(locationName).get(0));
         order.setPrice(0);
         order.setPaymentType(0);
-        order.setPayment(null);
+        order.setPayment(new Payment());
         order.setActive(false);
         order.setCompleted(false);
 
