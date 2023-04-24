95eca20225cd2fb1b9df9e6496ea3b284daaeacb
Dean Ivanov <dean.ivanov96@gmail.com>
2023-04-24 17:42:51 +0300

    Controller for Location Added

---
+++ src/main/java/com/example/carrental/controllers/LocationController.java
@@ -0,0 +1,35 @@
+package com.example.carrental.controllers;
+
+import com.example.carrental.models.Location;
+import com.example.carrental.services.LocationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LocationController {
+
+    private LocationService locationService;
+
+    @Autowired
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping(value = "/location-register")
+    public String showRegister(){
+        return "add-register";
+
+    }
+
+    @PostMapping(value = "/location-register")
+    public String registerLocation(@Valid @ModelAttribute("location") Location location) {
+        locationService.create(location);
+        return "index";
+
+    }
+
+}
