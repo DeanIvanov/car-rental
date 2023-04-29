aef179caa90dba7c7735291714cad2ec38978aae
Oryel <ory.gruper@gmail.com>
2023-04-29 17:58:01 +0300

    user active order and disable past dates

--- src/main/resources/static/js/disablePast.js
+++ src/main/resources/static/js/disablePast.js
@@ -1,5 +1,6 @@
 window.onload=function(){
 var today = new Date().toISOString().split('T')[0];
+document.getElementById('startDate').valueAsDate = new Date();
 document.getElementsByName("startDate")[0].setAttribute('min', today);
 document.getElementsByName("endDate")[0].setAttribute('min', today);
 }
